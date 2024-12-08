f7db132122d5097bcac2330494054f8652d002c1
ABHI <abhi202864@gmail.com>
2024-12-08 22:13:54 +0530

    Trapping Rain Wather Problem

--- Array/Array.java
+++ Array/Array.java
@@ -172,7 +172,34 @@ public class Array {
     return MS;
   }
 
-  
+  // TRAPPING RAIN WATHER
+  public static int trappedRainWather(int height[]) {
+    // CALCULATE LEFT MAX BOUNDARY
+    int[] leftMax = new int[height.length];
+    leftMax[0] = height[0];
+    for (int i = 1; i < height.length; i++) {
+      leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    }
+
+    // CALCULATE RIGHT MAX BOUNDARY
+    int[] rightMax = new int[height.length];
+    rightMax[height.length - 1] = height[height.length - 1];
+    for (int i = height.length - 2; i >= 0; i--) {
+      rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+    }
+
+    int trappedWather = 0;
+
+    // LOOP
+    for (int i = 0; i < height.length; i++) {
+      // watherLevel = main(leftMax bound, rightMax bound)
+      int watherLevel = Math.min(leftMax[i], rightMax[i]);
+      // trappedWather = watherLevel - height[i]
+      trappedWather += watherLevel - height[i];
+    }
+
+    return trappedWather;
+  }
 
   public static void main(String[] args) {
     // ---- NUMBER SEARCH
@@ -222,6 +249,8 @@ public class Array {
     // int[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
     // System.out.println("MAX = " + kadaneAlgorithm(arr));
 
-     
+    // TRAPPING RAIN WATHER
+    int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+    System.out.println(trappedRainWather(height));
   }
 }
